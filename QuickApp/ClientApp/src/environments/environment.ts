73e1a0596495941accc2b43039363b39d6327775
Chí Vỹ Nguyễn <chivy1204@gmail.com>
2021-05-11 20:30:47 +0700

    Publish to Production

--- QuickApp/ClientApp/src/environments/environment.ts
+++ QuickApp/ClientApp/src/environments/environment.ts
@@ -8,7 +8,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: true,
+    production: false,
     baseUrl: 'https://webapiquickapptest.eastus.cloudapp.azure.com', // Change this to the address of your backend API if different from frontend address
     tokenUrl: 'https://webapiquickapptest.eastus.cloudapp.azure.com/connect/token', // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
     loginUrl: '/login',
