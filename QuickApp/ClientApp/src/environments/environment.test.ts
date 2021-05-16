d7b1afe6936e6a1e0ff2bff28a1fd5395b66e4ff
Chí Vỹ Nguyễn <chivy1204@gmail.com>
2021-05-16 23:16:34 +0700

    Change config

--- QuickApp/ClientApp/src/environments/environment.test.ts
+++ QuickApp/ClientApp/src/environments/environment.test.ts
@@ -9,8 +9,8 @@
 
 export const environment = {
     production: false,
-    baseUrl: 'https://webapiquickapptest.eastus.cloudapp.azure.com', // Change this to the address of your backend API if different from frontend address
-    tokenUrl: 'https://webapiquickapptest.eastus.cloudapp.azure.com/connect/token', // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
+    baseUrl: 'https://vync.kereacentral.cloudapp.azure.com', // Change this to the address of your backend API if different from frontend address
+    tokenUrl: 'https://vync.kereacentral.cloudapp.azure.com/connect/token', // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
     loginUrl: '/login',
     requireHttps: false
 };
